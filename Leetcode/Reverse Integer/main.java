ab0997b10e47620425c5b2fc9de761a84e78d7d6
Nick Liu <Nickboy@Nicks-Mac-mini.local>
2015-04-11 12:50:32 -0500

    add reverse integer

---
+++ Leetcode/Reverse Integer/main.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int reverse(int x) {
+        
+        int result = 0;
+        while(x != 0){
+            //check overflow
+            if(Math.abs(result) > 214748364)
+                return 0;
+            result = result * 10 + x%10;
+            x = x/10;
+        }
+        return result;
+    
+        
+    }
+}
