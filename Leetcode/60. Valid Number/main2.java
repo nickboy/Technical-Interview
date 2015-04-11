3a5644165f20a55a149f1079c615ae790608bef0
Nick Liu <Nickboy@Nicks-Mac-mini.local>
2015-04-11 12:30:44 -0500

    First Commit

---
+++ Leetcode/60. Valid Number/main2.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public boolean isNumber(String s) {
+        int index=0, lengthOfString = s.length();
+        boolean isNumeric = false;
+        
+        //try to ignore all whitespaces
+        while(index<lengthOfString && Character.isWhitespace(s.charAt(index))) index++;
+        
+        //try to ignore all sign
+        if (index<lengthOfString && (s.charAt(index)== '+' || s.charAt(index) == '-')) index++;
+        
+        while(index<lengthOfString && Character.isDigit(s.charAt(index))) {
+            index ++;
+            isNumeric = true;
+        }
+        if(index<lengthOfString && ( s.charAt(index) == '.' )) {
+            index++;
+            while(index<lengthOfString && Character.isDigit(s.charAt(index))) {
+                index ++;
+                isNumeric = true;
+            }
+        }
+        
+        if((isNumeric == true) && index<lengthOfString && s.charAt(index) == 'e'){
+            index++;
+            isNumeric = false;
+            if(index<lengthOfString && (s.charAt(index) == '+' || s.charAt(index) == '-')) index++;
+            
+            while(index<lengthOfString && Character.isDigit(s.charAt(index))) {
+                index ++;
+                isNumeric = true;
+            }
+        }
+        
+        while(index<lengthOfString && Character.isWhitespace(s.charAt(index))) index++;
+        return isNumeric && (index == lengthOfString);
+        
+    }
+}
