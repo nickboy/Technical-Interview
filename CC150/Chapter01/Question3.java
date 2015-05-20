8a5c1d9518b5d8bab896b205df2abf3ca16023c7
Nick Liu <nickliu0211@gmail.com>
2015-05-19 23:50:48 -0500

    add Question3_1

--- CC150/Chapter01/Question3.java
+++ CC150/Chapter01/Question3.java
@@ -1,15 +1,22 @@
 package com.nickboy.cc150.chapter1
 
+import java.util.Arrays;
 /**
  * Given two strings, write a method to decide if one is a permutation of the
  * other.
  * 
  */
-//  O(1) space, O(n) time
-public class Question3 {
+//  O(1) space, O(nlogn) time, sort two strings then do the comparison
+public class Question3_1 {
 	public boolean permutation(String s, String t){
 		if(s.length != t.length)
 			return false;
-		
+		return sort(s).equals(sort(t));
+	}
+
+	public String sort(String s){
+		char[] content = s.toCharArray();
+		Arrays.sort(content);
+		return new String(content);
 	}
 }
