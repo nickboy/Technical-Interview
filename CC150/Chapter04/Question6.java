4d0013229f43283690b0162091dda056e82ee6c6
Nick Liu <nickliu0211@gmail.com>
2015-05-22 17:59:57 -0500

    add another style in Question4-5

---
+++ CC150/Chapter04/Question6.java
@@ -0,0 +1,53 @@
+package com.nickboy.cc150.chapter4;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+
+/**
+ * Write an algorithm to find the 'next' node (i.e., in-order successor) of a
+ * given node in a binary search tree. You may assume that each node has a link
+ * to its parent.
+ * 
+ */
+// O(1) space, O(n) time
+public class Question5 {
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	//recursive approach
+	boolean checkBST(TreeNode n) {
+		return checkBST(n, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	boolean checkBST(TreeNode n, int min, int max) {
+		if (n == null) return true;
+		if (n.val > max || n.val < min) return false;
+
+		if (!checkBST(n.left, min, n.val) || !checkBST(n.right, n.val, max)) {
+			return false;
+		}
+		return true;
+	}
+
+	//another style
+	private boolean validate(TreeNode node, int min, int max) {
+		if (node == null) {
+			return true;
+		}
+		if (min < node.val && node.val < max) {
+			return validate(node.left, min, node.val) && validate(node.right, node.val, max);
+		} else {
+			return false;
+		}
+	} 
+}
+
