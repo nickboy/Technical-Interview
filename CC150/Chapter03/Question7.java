7050eac8bee129e87499632f2ab0445ea532342c
Nick Liu <nickliu0211@gmail.com>
2015-05-22 14:49:16 -0500

    Add Question 3-7

---
+++ CC150/Chapter03/Question7.java
@@ -0,0 +1,98 @@
+package com.nickboy.cc150.chapter3;
+import java.util.Stack;
+
+
+/**
+ * An animal shelter holds only dogs and cats, and operates on a strictly
+ * "first in, first out" basis. People must adopt either the "oldest" (based on
+ * arrival time) of all animals at the shelter, or they can select whether they
+ * would prefer a dog or a cat (and will receive the oldest animal of that
+ * type). They cannot select which specific animal they would like. Create the
+ * data structures to maintain this system and implement operations such as
+ * enqueue, dequeueAny, dequeueDog and dequeueCat. You may use the built-in
+ * LinkedList data structure.
+ * 
+ */
+// space: all O(1)
+// time: enqueue O(1), dequeue O(1), dequeueCat O(n), dequeueDog O(n)
+
+public class Question7 {
+
+	public abstract class Animal {
+		private int order;
+		protected String name;
+		
+		public Animal(String n) {
+			name = n;
+		}
+
+		public void setOrder(int ord) {
+			order = ord;
+		}
+
+		public int getOrder() {
+			return order;
+		}
+
+		public boolean is OlderThan(Animal a)
+		{
+			return this.order < a.order; 
+		}
+	}
+
+	public class AnimalQueue { 
+		LinkedList<Dog> dogs = new LinkedList<Dog>();
+		LinkedList<Cat> cats = new LinkedList<Cat>();
+		private int order = 0; //timestamp
+
+		public void enqueue(Animal a) {
+			a.setOrder(order);
+			order++;
+
+			if (a instanceof Dog) 
+				dogs.addLast((Dog) a);
+			else if (a instanceof Cat)
+				cats.addList((Cat) a);
+		}
+
+		public Animal dequeueAny() {
+			if (dogs.size() == 0) {
+				return cats.poll();
+			} else if (cats.size() == 0) {
+				return dogs.poll();
+			}
+
+			Dog dog = dogs.peek();
+			Cat cat = cats.peek();
+
+			if (dog.isOlderThan(cat))
+				return dog.poll();
+			else
+				return cat.poll();
+		}
+
+		public Dog dequeueDogs() {
+			return dogs.getFirst();
+		}
+
+		public Cat dequeueCats() {
+			return cats.getFirst();
+		}
+
+	}
+
+	public class Dog extends Animal {
+		public Dog(String n) {
+			super(n);
+		}
+	}
+
+	public class Cat extends Animal {
+		public Cat(String n) {
+			super(n);
+		}
+	}
+
+
+}
+
