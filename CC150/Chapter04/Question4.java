3a82ce4de08c2cc21e09ab308d02f809a244ef84
Nick Liu <nickliu0211@gmail.com>
2015-05-22 16:00:11 -0500

    add Question4-4

---
+++ CC150/Chapter04/Question4.java
@@ -0,0 +1,51 @@
+package com.nickboy.cc150.chapter4;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Stack;
+
+
+/**
+ * Given a sorted (increasing order) array, write an algorithm to create a
+ * binary search tree with minimal height.
+ * 
+ */
+public class Question4 {
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	//recursive version
+	void createLevelLinkedList(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
+		if (root == null) return;
+
+		LinkedList<TreeNode> list = null;
+		if (lists.size() == level) { //this level is not in current array
+			//create a new list for current level
+			list = new LinkedList<TreeNode>();
+			lists.add(list);
+		} else {
+			list = lists.get(level);
+		}
+
+		list.add(root);
+		createLevelLinkedList(root.left, lists, level + 1);
+		createLevelLinkedList(root.right, lists, level + 1);
+	}
+
+	ArrayList<LinkedList<TreeNode>> createLevelLinkedList (TreeNode root) {
+		ArrayList<LinkedList<TreeNode>> lists = new ArrayList<LinkedList<TreeNode>>();
+		createLevelLinkedList(root, lists, 0);
+		return lists;
+	}
+
+
+}
+
